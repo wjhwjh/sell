9974235085da5b7eada64875ff782f25fedfd008
wjhwjh <1468017149@qq.com>
2020-09-02 15:22:15 +0800

    商家页面

--- src/components/seller/Seller.vue
+++ src/components/seller/Seller.vue
@@ -1,17 +1,20 @@
 <template>
-    <div class="box">
-      商家
+    <div class="seller">
+      <div class="seller-content"></div>
     </div>
 </template>
 <script>
 export default {
   created () {
-    console.log(this.$Axios)
-    // this.$Axios.get('')
+    // console.log(this.$Axios)
+    this.$Axios.get('/sellers')
+      .then((res) => {
+        console.log('这是数据', res)
+      })
   }
 }
 </script>
-<style lang="stylus" rel="stylesheet/stylus">
+<style lang="stylus" rel="stylesheet/stylus" scoped>
  .box
     width:100%
     height 50px
